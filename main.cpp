fef0502817b6f33c42631eb48ad5cb31959c2620
komiljon <k.qosimov@newuu.uz>
2023-10-29 20:15:31 +0500

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void func(int decimal, int base){
+    int remainder;
+    remainder = decimal;
+    if (remainder<base){
+        if (remainder<10){
+            cout << remainder;
+        } else if (remainder==10){
+            cout << "A";
+        } else if (remainder==11){
+            cout << "B";
+        } else if (remainder==12){
+            cout << "C";
+        } else if (remainder==13){
+            cout << "D";
+        } else if (remainder==14){
+            cout << "E";
+        } else if (remainder==15){
+            cout << "F";
+        } else {
+            cout << "only upto hexadecimal :(";
+        }
+    } else {
+        remainder = decimal % base;
+        if (remainder<10){
+            cout << remainder;
+        } else if (remainder==10){
+            cout << "A";
+        } else if (remainder==11){
+            cout << "B";
+        } else if (remainder==12){
+            cout << "C";
+        } else if (remainder==13){
+            cout << "D";
+        } else if (remainder==14){
+            cout << "E";
+        } else if (remainder==15){
+            cout << "F";
+        } else {
+            cout << "only upto hexadecimal :(";
+        }
+        func((decimal - remainder) / base,base);
+    }
+}
+
+int main(){
+    int decimal; int base;
+    cin >> decimal >> base ;
+    func(decimal,base);
     return 0;
 }
